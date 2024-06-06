dceefc7fc60a4a9e0a47856c80003ec4b9a036a9
Kah Wai <kwlee5354@gmail.com>
2024-06-07 02:12:53 +0800

    profile page content

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { RootNavigation } from "./navigation/root-stacks";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 
-
 const App = () => {
   return (
     <Provider store={store}>
